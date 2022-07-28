62e423ce70368a1fc4b54da7efabd48aa90e9f39
Supun Dasanayaka <supun.19@cse.mrt.ac.lk>
2022-07-28 18:52:01 +0530

    backend: user router/controller

--- server/routers/users.js
+++ server/routers/users.js
@@ -1,31 +1,10 @@
-const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/users');
 
-router.get(`/`,async (req, res) => {
-    const userList = await User.find();
-    if(!userList){
-        res.status(500).json({success: false})
-    }
-    res.send(userList);
-})
-
-router.post(`/`,(req,res)=>{
-    const user = new User({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-        type: req.body.type
-    })
-
-    user.save().then((createdUser => {
-        res.status(201).json(createdUser)
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
-})
+router.get(`/getUser`,userController.getUsers);
+router.post(`/addUser`,userController.addUser);
+router.put('/updateUser/:id', userController.updateUser);
+router.delete('/deleteUser/:id',userController.deleteUser);
 
 module.exports = router;
